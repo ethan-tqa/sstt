2353272096290999ac2f754b1a40ff4acabe880e
Ethan <ethan.tqa@gmail.com>
2020-10-22 23:22:27 +0800

    All the sweet stuff: Typescript, Tailwind CSS, Webpack minify

--- snowpack.config.js
+++ snowpack.config.js
@@ -7,9 +7,14 @@ module.exports = {
     '@snowpack/plugin-svelte',
     '@snowpack/plugin-dotenv',
     [
+      '@snowpack/plugin-build-script', 
+      {cmd: 'postcss', input: [".css"], output: [".css"]}
+    ],
+    [
       '@snowpack/plugin-run-script',
       {cmd: 'svelte-check --output human', watch: '$1 --watch', output: 'stream'},
     ],
+    [ '@snowpack/plugin-webpack', {} ]
   ],
   install: [
     /* ... */
